6448bf2065b71c0550302773d63815edfc98a986
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2023-09-17 13:31:07 -0500

    FriendsPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Navbar/Navbar';
 import Timetable from './Timetable/Timetable';
+import FriendsPage from './FriendsPage/FriendsPage';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <AppContext.Provider value={{}}>
         <Navbar/>
         {/* meter aca los componentes */}
+        <FriendsPage/>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Timetable/>}/>
---
+++ src/FriendsPage/FriendsPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "react-elastic-carousel";
+import "./FriendsPage.css";
+import flechaDer from '../assets/flechaDerecha.png';
+import flechaIzq from '../assets/felchaIzquierda.png';
+
+import { friends } from "./friends";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 3 },
+];
+
+function FriendsPage() {
+  const friendPairs = [];
+  for (let i = 0; i < friends.items.length; i += 2) {
+    friendPairs.push(friends.items.slice(i, i + 2));
+  }
+
+  return (
+    <>
+      <div className="FriendsPage">
+        <Carousel
+          breakPoints={breakPoints}
+          renderArrow={({ type, onClick }) => (
+            <button onClick={onClick} className={`custom-arrow-${type}`}>
+              {type === 'PREV' ? <img className="img1" src={flechaIzq} alt="flechaIzq" /> : <img className="img2" src={flechaDer} alt="flechaDer" />}
+            </button>
+          )}
+          paginationClassName="custom-pagination"
+          renderPagination={({ pages, activePage, onClick }) => (
+            <div className="pagination-container">
+              {pages.map((page) => (
+                <button
+                  key={page}
+                  onClick={() => onClick(page)}
+                  className={`custom-pagination-dot ${activePage === page ? "active" : ""
+                    }`}
+                ></button>
+              ))}
+            </div>
+          )}
+        >
+          {friendPairs.map((pair, index) => (
+            <div key={index} className="friend-pair-container">
+              {pair.map((friend, friendIndex) => (
+                <div key={friendIndex} className="friend-container">
+                  <div className="friends">
+                    <img
+                      className="friend-image"
+                      src={friend.image}
+                      alt={friend.name}
+                    />
+                  </div>
+                  <div className="friend-name">
+                    {friend.name}
+                  </div>
+                </div>
+              ))}
+              {index < friendPairs.length - 1 && <div className="friend-space"></div>}
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
+export default FriendsPage;
