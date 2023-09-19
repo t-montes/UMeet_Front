83a0d55c7559c5c0a2924947e665b20536ce7ed8
t-montes <t.montes@uniandes.edu.co>
2023-09-18 21:50:26 -0500

    add // to unused variable...

--- src/GroupsPage/GroupsPage.js
+++ src/GroupsPage/GroupsPage.js
@@ -1,6 +1,6 @@
 import './GroupsPage.css';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 import CardUsuario from '../GroupCard/GroupCard';
 import userImage1 from '../assets/icon7.png';
 import userImage2 from '../assets/icon8.png';
@@ -8,7 +8,7 @@ import userImage3 from '../assets/icon9.png';
 
 const GroupsPage = () => {
 
-    const { id } = useParams(); // id of the group
+    //const { id } = useParams(); // id of the group
 
     const cardsData = [
         { colorFondo: "#4A6FA5" , textoCentral: "Programación con tecnologías web" , imagenesPerfil: [userImage1, userImage2, userImage3]},
